1643971d7b498154e79f8b91b28b2f36c4e1fb7c
Ho Wun Mok <hmok11@my.centennialcollege.ca>
2023-11-07 10:51:34 -0500

    updated effects and responsive font size

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import { getImageUrl } from "../../utils";
 import styles from "./Hero.module.css";
+import Typed from "typed.js";
 
 export const Hero = () => {
+  const positions = React.useRef(null);
+  React.useEffect(() => {
+    const typed = new Typed(positions.current, {
+      strings: [
+        "Font-end Developer",
+        "Full-stack Developer",
+        "Freelance Photographer",
+      ],
+      typeSpeed: 90,
+      loop: true,
+    });
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
   return (
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Benny Mok</h1>
         <p className={styles.introduction}>
-          A passionate{" "}
-          <span className={styles.position}>Full-stack Developer</span> based in
-          Toronto, Canada
+          A passionate <span className={styles.position} ref={positions} />
+          based in Toronto, Canada
         </p>
 
         <a href="mailto:bennymok@protonmail.com" className={styles.contactBtn}>
