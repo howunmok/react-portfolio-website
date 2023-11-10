a64d83b305c0d5c5dccaf93e5884c37d94e34335
Ho Wun Mok <hmok11@my.centennialcollege.ca>
2023-11-09 21:45:12 -0500

    upload effects and resume

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,25 +9,39 @@ export const Contact = () => {
         <h2>Contact</h2>
         <p>Feel free to reach out!</p>
       </div>
-      <ul className={styles.links}>
-        <li className={styles.link}>
-          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
-          <a href="mailto:bennymok@protonmail.com">bennymok@protonmail.com</a>
-        </li>
-        <li className={styles.link}>
-          <img
-            src={getImageUrl("contact/linkedinIcon.png")}
-            alt="Linkedin icon"
-          />
-          <a href="https://www.linkedin.com/in/benny-mok">
-            linkedin.com/in/benny-mok
-          </a>
-        </li>
-        <li className={styles.link}>
-          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://github.com/howunmok">github.com/howunmok</a>
-        </li>
-      </ul>
+      <div className={styles.linksContainer}>
+        <ul className={styles.links}>
+          <li className={styles.link}>
+            <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+            <a href="mailto:bennymok@protonmail.com">bennymok@protonmail.com</a>
+          </li>
+          <li className={styles.link}>
+            <img
+              src={getImageUrl("contact/linkedinIcon.png")}
+              alt="Linkedin icon"
+            />
+            <a href="https://www.linkedin.com/in/benny-mok">
+              linkedin.com/in/benny-mok
+            </a>
+          </li>
+          <li className={styles.link}>
+            <img
+              src={getImageUrl("contact/githubIcon.png")}
+              alt="Github icon"
+            />
+            <a href="https://github.com/howunmok">github.com/howunmok</a>
+          </li>
+          <li>
+            <a
+              href="../assets/resume/Resume_BennyMok.pdf"
+              download
+              className={styles.downloadBtn}
+            >
+              Download Resume
+            </a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
