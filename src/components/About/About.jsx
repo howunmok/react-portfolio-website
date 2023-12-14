07d17b06d831c61635144ad49c1c3241e63e3e95
Ho Wun Mok <hmok11@my.centennialcollege.ca>
2023-12-14 01:25:33 -0500

    update education, update font size

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { getImageUrl } from "../../utils";
-import styles from "./About.module.css";
+import { getImageUrl } from "../../utils"
+import styles from "./About.module.css"
+import { TbCertificate } from "react-icons/tb"
+import { GiDiploma } from "react-icons/gi"
+import { RiGraduationCapFill } from "react-icons/ri"
 
 export const About = () => {
   return (
@@ -9,8 +11,8 @@ export const About = () => {
       <p className={styles.titleIntro}>
         I am a software engineering enthusiast specializing in web development,
         having recently completed the Diploma of Software Engineering Technician
-        program. With a focus on HTML, CSS, JavaScript, and Angular, I excel in
-        problem-solving and thrive in collaborative team environments.
+        program. With a focus on HTML, CSS, JavaScript, React and Angular, I
+        excel in problem-solving and thrive in collaborative team environments.
       </p>
       <div className={styles.content}>
         <img
@@ -20,37 +22,43 @@ export const About = () => {
         />
         <ul className={styles.aboutItems}>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" />
+            {/* <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor Icon" /> */}
+            <div className={styles.iconContainer}>
+              <TbCertificate className={styles.icon} />
+            </div>
             <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
-              <p>
-                I'm a front-end developer with experience in building responsive
-                and optimized website
-              </p>
+              <h3>Certificate of Completion - Web Development Training</h3>
+              <p>Cantek Program</p>
+              <small>Nov 2023 - Dec 2023</small>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon" />
+            {/* <img src={getImageUrl("about/serverIcon.png")} alt="Server Icon" /> */}
+            <div className={styles.iconContainer}>
+              <GiDiploma className={styles.icon} />
+            </div>
             <div className={styles.aboutItemText}>
-              <h3>Backend Developer</h3>
-              <p>
-                I have experience in developing fast and optimized back-end
-                systems and APIs
-              </p>
+              <h3>College Diploma of Software Engineering Technican</h3>
+              <p>Centennial College</p>
+              <small>Jan 2022 - May 2023</small>
             </div>
           </li>
           <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/uiIcon.png")} alt="UI Icon" />
+            {/* <img src={getImageUrl("about/uiIcon.png")} alt="UI Icon" /> */}
+            <div className={styles.iconContainer}>
+              <RiGraduationCapFill className={styles.icon} />
+            </div>
             <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
-              <p>
-                I have designed multiple landing pages and have created design
-                systems as well
-              </p>
+              <h3>
+                Bachelor of Science in Applied Science (Energy and Environment)
+                with Honours
+              </h3>
+              <p>Hong Kong Metropolitan University</p>
+              <small>Sep 2009 - Aug 2011</small>
             </div>
           </li>
         </ul>
       </div>
     </section>
-  );
-};
+  )
+}
